c7537a2bb0e11ec9f9b02028532aff64240495a7
Roberto Ramirez <rr4007@gmail.com>
2026-07-29 19:33:25 -0600

    Log Resultado

--- src/js/app.js
+++ src/js/app.js
@@ -98,17 +98,18 @@ function paginaSiguiente(){
 }
 
 async function consultarAPI(){
-
-    try {
-        const url = 'http://localhost:3000/api/servicios';
-        const resultado = await fetch(url);
-        const servicios = await resultado.json();       
-        console.log(servicios);
+    console.log('Hola Mundo');
+    
+    // try {
+    //     const url = 'http://localhost:3000/api/servicios';
+    //     const resultado = await fetch(url);
+    //     const servicios = await resultado.json();       
+    //     console.log(servicios);
          
-        // mostrarServicios(servicios);      
-    } catch (error) {
-        console.log(error);
-    }
+    //     // mostrarServicios(servicios);      
+    // } catch (error) {
+    //     console.log(error);
+    // }
 }
 
 function mostrarServicios(servicios){
